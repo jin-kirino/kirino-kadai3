fe0fb8cff63345991ff42cfd72fda999b4ec71da
jin-kirino <jinkirino@gmail.com>
2023-02-25 08:56:14 +0900

    #1 レイアウト完成

--- kirino-kadai3/ContentView.swift
+++ kirino-kadai3/ContentView.swift
@@ -8,12 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 左の入力値を格納する
+    @State private var leftText: String = ""
+    // 右の入力値を格納する
+    @State private var rightText: String = ""
+    // 左のtoggleを管理する
+    @State private var isLeftNegativeNumber: Bool = false
+    // 右のtoggleを管理する
+    @State private var isRightNegativeNumber: Bool = false
+    // 左の数字を格納する
+    @State private var leftNumber: String = "0"
+    // 右の数字を格納する
+    @State private var rightNumber: String = "0"
+    // 計算結果を格納
+    @State private var result: String = "0"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            VStack {
+                HStack {
+                    TextField("", text: $leftText)
+                        .frame(width: 100)
+                        .padding()
+                    TextField("", text: $rightText)
+                        .frame(width: 100)
+                        .padding()
+                }
+                HStack {
+                    Text("+")
+                    Toggle("", isOn: $isLeftNegativeNumber)
+                        .labelsHidden()
+                    Text("-")
+                    Spacer()
+                    Text("+")
+                    Toggle("", isOn: $isRightNegativeNumber)
+                        .labelsHidden()
+                    Text("-")
+                }
+                .tracking(10)
+                Button {
+                    print("タップされた")
+                } label: {
+                    Text("Button")
+                }
+                HStack {
+                    Spacer()
+                    Text(leftNumber)
+                    Text("+")
+                    Text(rightNumber)
+                }
+                .tracking(40)
+                .padding()
+                Text(result)
+                Spacer()
+            }
+            .frame(width: 250)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            Spacer()
         }
         .padding()
     }
