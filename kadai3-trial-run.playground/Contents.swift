81fdc10279f7b314ecc0edc2eb30ea54b3835992
jin-kirino <jinkirino@gmail.com>
2023-02-25 19:04:46 +0900

    メソッドをタプルで返す

--- kadai3-trial-run.playground/Contents.swift
+++ kadai3-trial-run.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// TextFieldに入力される値
+let leftText: String = "20"
+let rightText: String = "10"
+
+//func didTapAction(firstText: String, secondText: String) -> Int {
+//    guard let firstNumber = Int(firstText) else {
+//        return 0
+//    }
+//    guard let secondNumber = Int(secondText) else {
+//        return 0
+//    }
+//    return firstNumber + secondNumber
+//}
+
+let aaa = didTapAction(firstText: leftText, secondText: rightText)
+print(aaa)
+
+func didTapAction(firstText: String, secondText: String) -> (firstNumber: Int, secondNumber: Int) {
+    var firstNumber: Int = 0
+    var secondNumber: Int = 0
+    
+    guard let firstNumber = Int(firstText) else {
+        return (firstNumber, secondNumber)
+    }
+    guard let secondNumber = Int(secondText) else {
+        return (firstNumber, secondNumber)
+    }
+    return (firstNumber, secondNumber)
+}
+
+let ddd = didTapAction(firstText: leftText, secondText: rightText)
+print(ddd)
