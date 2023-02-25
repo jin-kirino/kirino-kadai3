1608fd14136ad603f2b7fba797a573b727fbcf15
jin-kirino <jinkirino@gmail.com>
2023-02-26 08:57:33 +0900

    #1 TextFieldとtoggleの重複コードをリファクタリング

--- kirino-kadai3/ContentView.swift
+++ kirino-kadai3/ContentView.swift
@@ -21,53 +21,58 @@ struct ContentView: View {
     // 右の数字を格納する
     @State private var rightNumber: Int = 0
     // 計算結果を格納
-    @State private var result: String = "0"
+    @State private var result: Int = 0
     
     var body: some View {
         HStack {
             VStack {
                 HStack {
-                    TextField("", text: $leftText)
-                        .frame(width: 100)
-                        .padding()
-                    TextField("", text: $rightText)
-                        .frame(width: 100)
-                        .padding()
-                }
-                HStack {
-                    Text("+")
-                    Toggle("", isOn: $isLeftNegativeNumber)
-                        .labelsHidden()
-                    Text("-")
-                    Spacer()
-                    Text("+")
-                    Toggle("", isOn: $isRightNegativeNumber)
-                        .labelsHidden()
-                    Text("-")
+                    inputTextView(inputText: $leftText, isMinus: $isLeftNegativeNumber)
+                    inputTextView(inputText: $rightText, isMinus: $isRightNegativeNumber)
+//                    TextField("", text: $leftText)
+//                        .frame(width: 100)
+//                        .padding()
+//                    TextField("", text: $rightText)
+//                        .frame(width: 100)
+//                        .padding()
+//                }
+//                HStack {
+//                    Text("+")
+//                    Toggle("", isOn: $isLeftNegativeNumber)
+//                        .labelsHidden()
+//                    Text("-")
+//                    Spacer()
+//                    Text("+")
+//                    Toggle("", isOn: $isRightNegativeNumber)
+//                        .labelsHidden()
+//                    Text("-")
                 }
                 .tracking(10)
                 Button {
                     // #2 ボタンタップ時のアクション
                     let chengedText = didTapAction(firstText: leftText, secondText: rightText)
                     // Int型に変換済み
-                    leftNumber = chengedText.firstNumber
-                    rightNumber = chengedText.secondNumber
+                    leftNumber = chengedText.firstNumber ?? 0
+                    rightNumber = chengedText.secondNumber ?? 0
                     // もし左のtogleがtrueだったらマイナスに変換
                     leftNumber = isLeftNegativeNumber ? -leftNumber : leftNumber
                     rightNumber = isRightNegativeNumber ? -rightNumber : rightNumber
-                    result = String(leftNumber + rightNumber)
+                    result = leftNumber + rightNumber
                     print("タップされた")
                 } label: {
                     Text("Button")
                 }
                 HStack {
+                    Spacer()
                     Text("\(leftNumber)")
+                    Spacer()
                     Text("+")
+                    Spacer()
                     Text("\(rightNumber)")
+                    Spacer()
                 }
-                .tracking(40)
                 .padding()
-                Text(result)
+                Text("\(result)")
                 Spacer()
             }
             .frame(width: 250)
@@ -76,18 +81,34 @@ struct ContentView: View {
         }
         .padding()
     }
-    private func didTapAction(firstText: String, secondText: String) -> (firstNumber: Int, secondNumber: Int) {
-        let firstNumber: Int = 0
-        let secondNumber: Int = 0
+    
+    private func didTapAction(firstText: String, secondText: String) -> (firstNumber: Int?, secondNumber: Int?) {
         // 数字以外は返す
-        guard let firstNumber = Int(firstText) else {
-            return (firstNumber, secondNumber)
-        }
-        guard let secondNumber = Int(secondText) else {
-            return (firstNumber, secondNumber)
+        guard let firstNumber = Int(firstText), let secondNumber = Int(secondText) else {
+            return (nil, nil)
         }
         // 2つとも数字の時のみ、Int型に変換
         return (firstNumber, secondNumber)
+    }
+}
+
+// TextFieldとtoggleのView
+struct inputTextView: View {
+    @Binding var inputText: String
+    @Binding var isMinus: Bool
+    
+    var body: some View {
+        VStack {
+            TextField("", text: $inputText)
+                .frame(width: 100)
+                .padding()
+            HStack {
+                Text("+")
+                Toggle("", isOn: $isMinus)
+                    .labelsHidden()
+                Text("-")
+            }
+        }
     }
 }
 
